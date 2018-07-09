3342b2cc7d0c506b58dbfbfef3fea914824d30c3
Yannis Zarkadas <yanniszarkadas@gmail.com>
2018-07-09 23:00:02 +0300

    add code

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,70 @@
 package cmd
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sync"
+	"syscall"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"github.com/yanniszark/kubernetes-operator-demo/pkg/controller"
+)
+
+func main() {
+
+	// Set logging output to stdout
+	log.SetOutput(os.Stdout)
+
+	// Create a channel to receive OS signals
+	sigs := make(chan os.Signal, 1)
+	// Create a channel to receive stop signal
+	stop := make(chan struct{})
+
+	// Register the sigs channel to receive SIGTERM
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	// Goroutines need to add themselves to this to be waited on when they finish
+	wg := &sync.WaitGroup{}
+
+	// Register CMD flag indicating if the controller is running outside of Kubernetes
+	runOutsideCluster := flag.Bool("run-outside-cluster", false, "Set this flag when running outside of the cluster.")
+	flag.Parse()
+
+	// Create clientset for interacting with the kubernetes cluster
+	clientset, err := newClientSet(*runOutsideCluster)
+
+	// Initialize controller
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	controller.NewNamespaceController(clientset).Run(stop, wg)
+
+	// Wait for signals (this hangs until a signal arrives)
+	<-sigs
+	log.Printf("Shutting down...")
+}
+
+func newClientSet(runOutsideCluster bool) (*kubernetes.Clientset, error) {
+	kubeConfigLocation := ""
+
+	if runOutsideCluster == true {
+		homeDir := os.Getenv("HOME")
+		kubeConfigLocation = filepath.Join(homeDir, ".kube", "config")
+	}
+
+	// Use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigLocation)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return kubernetes.NewForConfig(config)
+
+}
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,1 +1,105 @@
 package controller
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+// NamespaceController watches the kubernetes api for changes to namespaces and
+// creates a Rolebinding for that particular namespace.
+type NamespaceController struct {
+	namespaceInformer cache.SharedIndexInformer
+	kclient *kubernetes.Clientset
+}
+
+// Run starts the process for listening for namespace changes and acting upon those changes.
+func (c *NamespaceController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
+
+	// When this function completes, mark the go function as done
+	defer wg.Done()
+
+	// Increment wait group before creating a new goroutine
+	wg.Add(1)
+
+	// Execute goroutine
+	go c.namespaceInformer.Run(stopCh)
+
+	// Wait until we receive a stop signal
+	<-stopCh
+}
+
+//NewNamespaceController creates a new NamespaceController
+func NewNamespaceController(kclient *kubernetes.Clientset) *NamespaceController {
+
+	namespaceController := &NamespaceController{}
+
+	// Create informer for watching Namespaces
+	namespaceInformer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return kclient.CoreV1().Namespaces().List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return kclient.CoreV1().Namespaces().Watch(options)
+			},
+		},
+			&v1.Namespace{},
+			3*time.Minute,
+			cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
+	)
+
+
+	namespaceInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: namespaceController.createRoleBinding,
+	})
+
+	namespaceController.kclient = kclient
+	namespaceController.namespaceInformer = namespaceInformer
+
+	return namespaceController
+}
+
+func (c *NamespaceController) createRoleBinding(obj interface{}) {
+	namespaceObj := obj.(*v1.Namespace)
+	namespaceName := namespaceObj.Name
+
+	roleBinding := &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("ad-kubernetes-%s", namespaceName),
+			Namespace: namespaceName,
+		},
+		Subjects: []rbacv1.Subject{
+			rbacv1.Subject{
+				Kind: "Group",
+				Name: fmt.Sprintf("ad-kubernetes-%s", namespaceName),
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "edit",
+		},
+	}
+
+	_, err := c.kclient.RbacV1().RoleBindings(namespaceName).Create(roleBinding)
+
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed to create Role Binding: %s", err.Error()))
+	} else {
+		log.Println(fmt.Sprintf("Created AD RoleBinding for Namespace: %s", roleBinding.Name))
+	}
+}
